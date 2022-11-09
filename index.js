d38a18aa3809f1d5f303ac54fd1f7ac13819667d
mirzanahid <mirzasaikat3@gmail.com>
2022-11-09 19:57:35 +0600

    service details added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -24,7 +24,7 @@ async function run() {
 
     try {
         const serviceCollection = client.db('pixelsPhotography').collection('services')
-        app.get('/homeservices', async (req, res) => {
+        app.get('/home_services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.limit(3).toArray();
@@ -36,6 +36,15 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services);
         })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const serviceDetails = await serviceCollection.findOne(query);
+            res.send(serviceDetails)
+        })
+
+        //
     }
     finally {
 
